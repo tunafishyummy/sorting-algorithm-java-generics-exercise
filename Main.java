3c8623e19a4038e77dc50a0468cb47797552ca34
tunafishyummy <adinoellamio@gmail.com>
2026-09-02 11:14:02 +0800

    Just the start of it and prompting the user

---
+++ Main.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Sorting Algorithm");
+        System.out.println("Sort integers or strings? (Enter 'int' or 'str')");
+        String firstinput = scanner.nextLine();
+
+        
+    }
+}
