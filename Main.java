80b77714e4df504f0e756849e52d40a0e6600957
tunafishyummy <adinoellamio@gmail.com>
2026-09-02 11:26:15 +0800

    gitignore

--- Main.java
+++ Main.java
@@ -4,15 +4,17 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Sorting Algorithm");
+        System.out.println("===============Sorting Algorithm===============\n");
         System.out.println("Sort integers or strings? (Enter 'int' or 'str')");
-        
+
         String firstinput = scanner.nextLine();
 
         if (firstinput.equals("int")) {
             System.out.println("Enter integers separated by spaces:");
+            String input = scanner.nextLine();
         } else if (firstinput.equals("str")) {
             System.out.println("Enter strings separated by spaces:");
+            String input = scanner.nextLine();
         } else {
             System.out.println("Invalid input. Please enter 'int' or 'str'.");
         }
