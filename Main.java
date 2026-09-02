5aefb95e7df76017a9aa32f033839f04d2b3d6c0
tunafishyummy <adinoellamio@gmail.com>
2026-09-02 11:18:18 +0800

    Just the start of it and prompting the user 2

--- Main.java
+++ Main.java
@@ -3,10 +3,19 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+
         System.out.println("Sorting Algorithm");
         System.out.println("Sort integers or strings? (Enter 'int' or 'str')");
+        
         String firstinput = scanner.nextLine();
 
-        
+        if (firstinput.equals("int")) {
+            System.out.println("Enter integers separated by spaces:");
+        } else if (firstinput.equals("str")) {
+            System.out.println("Enter strings separated by spaces:");
+        } else {
+            System.out.println("Invalid input. Please enter 'int' or 'str'.");
+        }
+
     }
 }
